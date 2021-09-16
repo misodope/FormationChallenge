10b1f1899050fad5b9169960b00684e6b1ec3ee9
Jerry Chen <misodope@cap-rx.com>
2021-09-16 16:49:39 -0400

    Boop

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,53 +17,92 @@ export class GildedRose {
         this.items = items;
     }
 
-    updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
+    updateItems() {
+        this.items.map((item) => {
+            const type = this.getItemType(item);
+
+            if (type == 'legendary') return item;
+
+            // Immediately decrease Sellin
+            this.decreaseSellin(item);
+
+            if (type === "backstage") {
+                this.updateBackstageItem(item);
+            } else if (type === "conjured") {
+                this.decreaseQuality(item, 2);
+            } else if (type === "aged") {
+                this.increaseQuality(item);
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+                this.decreaseQuality(item);
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
+
+            return item;
+        });
 
         return this.items;
+    }
+
+    increaseQuality(item: Item, amount = 1) {
+        const quality = item.quality + amount;
+
+        if (quality > 50) {
+            item.quality = 50;
+        } else {
+            item.quality = quality;
+        }
+
+        return item.quality;
+    }
+
+    decreaseQuality(item: Item, amount = 1) {
+        const quality = item.quality - amount;
+
+        if (quality < 0) {
+            item.quality = 0;
+        } else {
+            item.quality = quality;
+        }
+
+        return item.quality;
+    }
+
+    decreaseSellin(item: Item) {
+        item.sellIn = item.sellIn - 1;
+
+        return item.sellIn;
+    }
+
+    updateBackstageItem(item: Item) {
+        if (item.sellIn < 0) {
+            this.decreaseQuality(item, item.quality);
+        } else if (item.sellIn > 5 && item.sellIn <= 10) {
+            // Quality increased 2x if sellin is greater than 5 and <= 10
+            this.increaseQuality(item, 2);
+        } else if (item.sellIn <= 5) {
+            this.increaseQuality(item, 3);
+        } else {
+            this.increaseQuality(item);
+        }
+    }
+
+    getItemType(item: Item) {
+        const typeMap = {
+            Aged: "aged",
+            Normal: "normal",
+            Conjured: "conjured",
+            Legendary: "legendary",
+            Backstage: "backstage",
+        }
+
+        if (item.quality === 80) return typeMap.Legendary;
+
+        const splitName = item.name.split(' ');
+        const type = splitName[0];
+
+        if (typeMap[type]) {
+            return typeMap[type];
+        } else {
+            return typeMap.Normal;
+        }
     }
 }
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -4,9 +4,32 @@ import { Item, GildedRose } from '../app/gilded-rose';
 describe('Gilded Rose', function () {
 
     it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+        const gildedRose = new GildedRose([
+            new Item('foo', 30, 1),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Aged Brie", 2, 0),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40),
+            new Item("Conjured Mana Cake", 3, 6),
+        ]);
 
+        const mockData = [
+            new Item('foo', 29, 0), // sellin 29, quality 0
+            new Item('Backstage passes to a TAFKAL80ETC concert', 4, 13), // quality 13
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Aged Brie", 1, 1),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+            new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
+            new Item("Conjured Mana Cake", 2, 4),
+        ];
+
+        const items = gildedRose.updateItems();
+
+        expect(items).to.deep.equal(mockData);
+    });
 });
